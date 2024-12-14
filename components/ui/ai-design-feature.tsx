9a9922ae99da97ef200e120528709bab56f68dfe
abdullah-25 <mabdullah@tvo.org>
2024-12-14 17:25:51 -0500

    remove extra space

--- components/ui/ai-design-feature.tsx
+++ components/ui/ai-design-feature.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import Image from 'next/image'
+
 import Link from 'next/link'
 import { motion, AnimatePresence } from 'framer-motion'
 import { Button } from "@/components/ui/button"
